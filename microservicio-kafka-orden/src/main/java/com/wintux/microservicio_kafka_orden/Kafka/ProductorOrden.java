77a9a6a2ca35a4062fd7dca10dc6c9d3c87540af
WinTux <rusok.software@gmail.com>
2024-11-22 21:27:08 -0400

    Configurando servicios consumidores

--- microservicio-kafka-orden/src/main/java/com/wintux/microservicio_kafka_orden/Kafka/ProductorOrden.java
+++ microservicio-kafka-orden/src/main/java/com/wintux/microservicio_kafka_orden/Kafka/ProductorOrden.java
@@ -5,6 +5,9 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,5 +20,12 @@ public class ProductorOrden {
         this.topic = topic;
         this.kafkaTemplate = kafkaTemplate;
     }
-    // metodo enviarMensaje()
+    public void enviarMensaje(OrdenEvento evento){
+        LOGGER.info(String.format("Evento publicado desde microservicio Orden: %s",evento));
+        Message<OrdenEvento> mensaje = MessageBuilder
+                .withPayload(evento)
+                .setHeader(KafkaHeaders.TOPIC,topic.name())
+                .build();
+        kafkaTemplate.send(mensaje);
+    }
 }
